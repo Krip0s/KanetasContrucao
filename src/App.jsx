9339ae3ba58e84220174f751ae71308e1f12d248
Krip0s <mcaires229@gmail.com>
2025-12-24 12:34:44 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import KanetasDMais from './assets/Natal-Photoroom.png'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <style>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" />
+      </style>
+      <div>
+        <a >
+          <img src={KanetasDMais} className="logo" alt="KanetasD+Logo" width={640} height={236} />
+        </a>
+      </div>
+      <h1>Loja online da KanetasD+  </h1>
+      <h1>estará disponivel em breve!</h1>
+    </>
+  )
+}
+
+export default App
