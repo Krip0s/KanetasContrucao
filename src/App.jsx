cc428c30b581e97277f5254c14f7cade6ed0a61c
Krip0s <mcaires229@gmail.com>
2026-04-08 11:28:30 -0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,6 @@ function App() {
 
   return (
     <>
-      <style>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" />
-      </style>
       <div>
         <a >
           <img src={KanetasDMais} className="logo" alt="KanetasD+" width={800} />
